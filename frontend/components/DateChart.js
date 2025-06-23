a4e9f2f5e2b9d33d5d69ff5fb296433fe3a4b957
dzulfi <dzulfiqar.zahid13@gmail.com>
2025-06-23 11:32:40 +0700

    update grafik, filter berdasarkan camera yang dipilih

--- frontend/components/DateChart.js
+++ frontend/components/DateChart.js
@@ -14,13 +14,36 @@ export default function DateChart({ logs }) {
     const [data, setData] = useState([]);
     const [startDate, setStartDate] = useState('');
     const [endDate, setEndDate] = useState('');
+    const [cameraId, setCameraId] = useState('');
+    const [cameraList, setCameraList] = useState([]);
+
+    useEffect(() => {
+        async function fetchCameras() {
+            try {
+                const res = await fetch('http://localhost:5000/api/cameras');
+                if (res.ok) {
+                    const cams = await res.json();
+                    setCameraList(cams);
+                } else {
+                    console.log('gagal akses camera');
+                    console.warn('Response gagal. Mungkin backend tidak aktif')
+                }
+            } catch (error) {
+                window.alert('Silahkan hidupkan python dulu');
+                console.warn('Tidak bisa terhubung ke backend. Pastikan server python aktif');
+            }
+        }
+        fetchCameras();
+    }, []);
 
     useEffect(() => {
         const countByDate = {};
         logs.forEach(log => {
             const [date] = log.start_time.split(' ');
             const inRange = (!startDate || date >= startDate) && (!endDate || date <= endDate);
-            if (inRange) {
+            const matchesCamera = !cameraId ||log.cameraId ||log.camera_id === cameraId;
+
+            if (inRange && matchesCamera) {
                 countByDate[date] = (countByDate[date] || 0) + 1;
             }
         });
@@ -30,17 +53,26 @@ export default function DateChart({ logs }) {
             .sort((a,b) => new Date(a.date) - new Date(b.date));
         
         setData(chartData);
-    }, [logs, startDate, endDate]);
+    }, [logs, startDate, endDate, cameraId]);
 
     return (
         <div style={{ flex: 1, minWidth: '300px', height: 450 }}>
-            <h3>Grafik per bulan</h3>
+            <h3>Grafik per hari</h3>
             <div style={{ marginBottom: '1rem' }}>
                 <label>
                     Dari: <input type='date' value={startDate} onChange={e => setStartDate(e.target.value)} />
                 </label>
                 <label style={{ marginLeft: '1rem' }}>
                     Sampai: <input type='date' value={endDate} onChange={e => setEndDate(e.target.value)} />
+                </label>
+                <label style={{ marginLeft: '1rem' }}>
+                    Kamera:
+                    <select value={cameraId} onChange={e => setCameraId(e.target.value)}>
+                        <option value=''>Semua</option>
+                        {cameraList.map(cam => (
+                            <option key={cam._id} value={cam._id}> {cam.name_camera} </option>
+                        ))}
+                    </select>
                 </label>
             </div>
             <ResponsiveContainer width="100%" height="80%">
--- frontend/components/MonthChart.js
+++ frontend/components/MonthChart.js
@@ -12,32 +12,69 @@ import {
 export default function MonthChart({ logs }) {
     const [data, setData] = useState([]);
     const [selectedYear, setSelectedYear] = useState('');
+    const [selectedCamera, setSelectedCamera] = useState('');
+    const [cameraOptions, setCameraOptions] = useState([]);
+
+    const cameraMatch = !selectedCamera || selectedCamera === log.camera_id;
+
+    useEffect(() => {
+        async function fetchCameras() {
+            try {
+                const res = await fetch('http://localhost:5000/api/cameras');
+                if (res.ok) {
+                    const cams = await res.json();
+                    setCameraOptions(cams)
+                } else {
+                    console.log('gagal akses camera');
+                    console.warn('Response gagal. Mungkin backend tidak aktif')
+                }
+            } catch (error) {
+                window.alert('Silahkan hidupkan python dahulu');
+                console.warn('tidak bisa terhubung ke backend. pastikan server python aktif');
+            }
+        }
+        fetchCameras();
+    }, []);
 
     useEffect(() => {
         const countByMonth = {};
+        const months = Array.from({ length: 12 }, (_, i) => `${i + 1}`.padStart(2, '0'))
+
         logs.forEach(log => {
             const [date] = log.start_time.split(' ');
             const [year, month] = date.split('-');
-            const label = `${year}-${month}`;
+            // const label = `${year}-${month}`;
+            const camId = log.camera_id;
 
-            if (!selectedYear || selectedYear === year) {
+            const label = `${year}-${month}`;
+            const yearMatch = !selectedYear || selectedYear === year;
+            const cameraMatch = !selectedCamera ||selectedCamera === camId;
+
+            if (yearMatch && cameraMatch) {
                 countByMonth[label] = (countByMonth[label] || 0) + 1;
             }
         });
 
-        const chartData = Object.entries(countByMonth)
-            .map((month, count) => ({ month, count }))
-            .sort((a, b) => new Date(a.month + "-01") - new Date(b.month + "=01"));
-
-            setData(chartData);
-    }, [logs, selectedYear]);
+        // const chartData = Object.entries(countByMonth)
+        //     .map((month, count) => ({ month, count }))
+        //     .sort((a, b) => new Date(a.month + "-01") - new Date(b.month + "=01"));
+        const monthFull = months.map(month => {
+            const key = `${selectedYear || new Date().getFullYear()}-${month}`;
+            return {
+                month: key,
+                count: countByMonth[key] || 0
+            }
+        })
+            setData(monthFull);
+    }, [logs, selectedYear, selectedCamera]);
 
     const years = Array.from(new Set(logs.map(log => log.start_time.split('-')[0])));
 
     return (
         <div style={{ flex: 1, minWidth: '300px', height: 450 }}>
         <h3>Aktivitas per Bulan</h3>
-        <div style={{ marginBottom: '1rem' }}>
+
+        <div style={{ display: 'flex', gap: '1rem', marginBottom: '1rem', flexWrap: 'wrap' }}>
             <label>
             Tahun:{" "}
             <select value={selectedYear} onChange={e => setSelectedYear(e.target.value)}>
@@ -46,6 +83,15 @@ export default function MonthChart({ logs }) {
                 <option key={year} value={year}>{year}</option>
                 ))}
             </select>
+            </label>
+            <label>
+                Kamera:{" "}
+                <select value={selectedCamera} onChange={e => setSelectedCamera(e.target.value)}>
+                    <option value="">Semua</option>
+                    {cameraOptions.map(cam => (
+                        <option key={cam._id} value={cam._id}> {cam.name_camera} </option>
+                    ))}
+                </select>
             </label>
         </div>
         <ResponsiveContainer width="100%" height="80%">
